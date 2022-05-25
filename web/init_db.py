fce930958bd0b2f3fda236e654ec74cd7b0acc29
almiluk <almiluk@gmail.com>
2022-05-25 09:34:12 +0500

    automatically update db schema

--- web/init_db.py
+++ web/init_db.py
@@ -1,12 +1,25 @@
 import sqlite3
+from os.path import exists
+from glob import glob
 
 
 conn = sqlite3.connect("data/buses.db")
 cur = conn.cursor()
-with open("startup.sql") as startup_script:
-    script_str = startup_script.read()
-    cur.executescript(script_str)
-    conn.commit()
+try:
+    cur.execute("SELECT * FROM db_ver LIMIT 1;")
+    ver = int(cur.fetchall()[0][0])
+except:
+    ver = 0
+
+max_ver = len(glob("sql/migrate*.sql"))
+print(f"Current db version is {ver}, maximum version is {max_ver}")
+for cur_ver in range(ver + 1, max_ver + 1):
+    with open(f"sql/migrate{cur_ver}.sql") as startup_script:
+        script_str = startup_script.read()
+        cur.executescript(script_str)
+        cur.execute(f"UPDATE db_ver SET db_ver = {ver + 1}")
+        conn.commit()
+        print(f"Updated to version {cur_ver}")
 
 conn.close()
 
