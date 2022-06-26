93c71fe32a8b62b29669c8979443a6c57bf0a66f
Ondatr59 <almiluk@gmail.com>
2022-06-26 18:58:32 +0500

    backend: search bus stops by names, but not by ids

--- web/path_finder.py
+++ web/path_finder.py
@@ -1,5 +1,5 @@
-from models import BusStop
-
+from peewee import JOIN
+from models import Stop, BusStop
 
 def direct_route(dep_stop_id: int, arr_stop_id: int):
     """Find direct bus routes between two stops
@@ -9,7 +9,9 @@ def direct_route(dep_stop_id: int, arr_stop_id: int):
     :return: list of bus ids
     """
 
-    buses = BusStop.select(BusStop.bus_id).where(BusStop.stop_id == dep_stop_id)\
-            .intersect(BusStop.select(BusStop.bus_id).where(BusStop.stop_id == arr_stop_id))
+    dep_stop_name = Stop.get(Stop.stop_id == dep_stop_id).name
+    arr_stop_name = Stop.get(Stop.stop_id == arr_stop_id).name
+    buses = BusStop.select(BusStop.bus_id).join(Stop, JOIN.INNER).where(Stop.name == dep_stop_name)\
+            .intersect(BusStop.select(BusStop.bus_id).join(Stop, JOIN.INNER).where(Stop.name == arr_stop_name))
 
     return [b.bus_id.number for b in buses]
